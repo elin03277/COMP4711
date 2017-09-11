50a87cc96b27a6680524723ac07e3a2b0afd8e5a
Eric Lin <elin03277@gmail.com>
2017-09-10 17:06:27 -0700

    Added student objects and added comments

--- Student.php
+++ Student.php
@@ -15,6 +15,7 @@
 
 class Student {
 
+    //constructor initializes variables
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -22,14 +23,17 @@ class Student {
         $this->grades = array();
     }
 
+    //function to add an address to an array of emails
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
 
+    //function to add grade to an array of grades
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    //function to calculate the average grade of all grades
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -37,13 +41,18 @@ class Student {
         return $total / count($this->grades);
     }
 
+    //function to print out each student's information
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' (' . $this->average() . ")\n";
+
         foreach ($this->emails as $which => $what)
             $result .= $which . ': ' . $what . "\n";
-        $result .= "\n";
-        return '<pre>' . $result . '</pre>';
 
+        $result .= "\n";
+
+        return '<pre>' . $result . '</pre>';
     }
 }
+
+?>
--- index.php
+++ index.php
@@ -1,14 +1,67 @@
 <!DOCTYPE html>
 
+<!--
+ Created by PhpStorm.
+ User: Eric Lin
+ Date: 2017-09-08
+ Time: 11:46 AM
+-->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
-
     <body>
-    <?php
-        echo "Hi there";
-    ?>
+        <?php
+
+        //including the student file
+        include('student.php');
+
+        //initializing students variable
+        $students = array();
+
+        //adding the first student object to the array of students
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+
+        //adding the second student object to the array of students
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+
+        //adding the third student object to the array of students
+        $third = new Student();
+        $third->surname = "Lin";
+        $third->first_name = "Hsien-Chia";
+        $third->add_email('home','elin03277@gmail.com');
+        $third->add_grade(76);
+        $third->add_grade(82);
+        $third->add_grade(70);
+        $students['h789'] = $third;
+
+        //sorting the student objects in alphabetical order by their first name
+        ksort($students);
+
+        //printing out each student's information
+        foreach ($students as $student)
+            echo $student->toString();
+
+
+        ?>
     </body>
 </html>
